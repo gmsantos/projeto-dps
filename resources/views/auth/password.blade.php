88072912b536cba9374a3fa2346f16de00076b01
gmsantos <gabriel.ms1@hotmail.com>
2017-12-01 01:35:49 -0200

    translate some views

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -4,13 +4,13 @@
 
 @section('content')
 <div class="panel panel-default">
-    <div class="panel-heading">Reset Password</div>
+    <div class="panel-heading">Reset Senha</div>
     <div class="panel-body">
         <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
             <div class="form-group">
-                <label class="col-md-4 control-label">E-Mail Address</label>
+                <label class="col-md-4 control-label">E-Mail</label>
                 <div class="col-md-6">
                     <input type="email" class="form-control" name="email" value="{{ old('email') }}">
                 </div>
@@ -19,7 +19,7 @@
             <div class="form-group">
                 <div class="col-md-6 col-md-offset-4">
                     <button type="submit" class="btn btn-primary">
-                        Send Password Reset Link
+                        Enviar Link de reset de senha
                     </button>
                 </div>
             </div>
